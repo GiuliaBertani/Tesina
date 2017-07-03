c690d9d83fc7da0a97399f720fb3e93b92b5b7cb
Giulia Bertani <GiuliaBertani@MacBook-Pro-di-Giulia.local>
2017-07-03 20:34:47 +0200

    Fine

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,75 +1,9 @@
-
-function scroll(eID, speed) {
-
-  var windowObject = window;
-
-  var windowPos = windowObject.pageYOffset;
-
-  //var pointer = toElement.getAttribute('href').slice(1);
-
-  var elem = document.getElementById(eID);
-
-  var elemOffset = elem.offsetTop;
-
-
-
-  if (windowPos > elemOffset) {
-
-    speed = (windowPos - elemOffset) / speed;
-
-  }
-
-  if (windowPos < elemOffset) {
-
-    speed = (elemOffset - windowPos) / speed;
-
-  }
-
-  var counter = setInterval(function () {
-
-    windowPos;
-
-
-
-    if (windowPos > elemOffset) { // from bottom to top
-
-      windowObject.scrollTo(0, windowPos);
-
-      windowPos -= speed;
-
-
-
-      if (windowPos <= elemOffset) { // scrolling until elemOffset is higher than scrollbar position, cancel interval and set scrollbar to element position
-
-        clearInterval(counter);
-
-        windowObject.scrollTo(0, elemOffset);
-
-      }
-
-    } else { // from top to bottom
-
-      windowObject.scrollTo(0, windowPos);
-
-      windowPos += speed;
-
-
-
-      if (windowPos >= elemOffset) { // scroll until scrollbar is lower than element, cancel interval and set scrollbar to element position
-
-        clearInterval(counter);
-
-        windowObject.scrollTo(0, elemOffset);
-
-      }
-
-    }
-
-
-
-  }, 1
-
-  );
-
-}
-
+$(function () {
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 60
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+});
